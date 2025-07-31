9a0b5db64a27c6e7122b2bb5fa349418be0d27f4
DonaldKisaka <kisakadonald1@gmail.com>
2025-07-31 12:12:59 +0300

    controllers

---
+++ server/controllers/messageController.js
@@ -0,0 +1,62 @@
+import Message from "../models/messageModel.js";
+
+export const createMessage = async (req, res, next) => {
+    try {
+        const { message, userId } = req.body;
+
+        if (!newMessage || !userId) {
+            return res.status(400).json({
+                success: false,
+                message: "Message and userId are required"
+            });
+        }
+
+        const newMessage = new Message({
+            message,
+            userId
+        });
+
+        await newMessage.save();
+
+        res.status(201).json({
+            success: true,
+            data: newMessage
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getMessages = async (req, res, next) => {
+    try {
+
+        const messages = await Message.find().populate("userId");
+
+        res.status(200).json({
+            success: true,
+            data: messages
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getMessage = async (req, res, next) => {
+    try {
+        const message = await Message.findById(req.params.id).populate("userId");
+
+        if (!message) {
+            return res.status(404).json({
+                success: false,
+                message: "Message not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: message
+        });
+    } catch (error) {
+        next(error);
+    }
+}
---
+++ server/controllers/sentimentController.js
@@ -0,0 +1,19 @@
+import { analyzeSentiment } from "../utils/sentimentalService.js";
+
+const analyzeSentimentController = async (req, res, next) => {
+    try {
+        const { message } = req.body;
+        if (!message) {
+            return res.status(400).json({ error: "Message is required" });
+        }
+        const sentiment = await analyzeSentiment(message);
+        res.status(200).json({
+            success: true,
+            data: sentiment
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default analyzeSentimentController;

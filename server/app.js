4de5524ca929bb6bf07421e08781206ca4f0e115
DonaldKisaka <kisakadonald1@gmail.com>
2025-08-05 14:31:11 +0300

    !

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.CLIENT_URL || "http://localhost:3000",
+        origin: process.env.CLIENT_URL,
         methods: ["GET", "POST"],
     }
 });

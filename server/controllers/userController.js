d2b78421f35c10da27f64021502e24eb98fbce0c
DonaldKisaka <kisakadonald1@gmail.com>
2025-08-06 14:24:55 +0300

    user changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,40 @@
 import User from "../models/User.js";
 
+export const registerUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists with this email"
+            });
+        }
+
+        const user = await User.create({
+            email,
+            password
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            data: {
+                id: user._id,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+        next(error);
+    }
+}
+
+
 export const getUsers = async (req, res, next) => {
     try {
         const users = await User.find();
@@ -27,6 +62,26 @@ export const getUser = async (req, res, next) => {
         res.status(200).json({
             success: true,
             data: user
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User deleted successfully"
         })
     } catch (error) {
         next(error);
--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        required: [true, 'User Name is Required'],
-        type: String,
-    },
     email: {
         type: String,
         unique: true,
         required: [true, 'User Email is Required'],
         match: [/\S+@\S+\.\S+/, 'Please use a valid email address'],
     },
-}, { timestamps: true});
+    password: {
+        type: String,
+        required: [true, 'Password is Required'],
+        minlength: [6, 'Password must be at least 6 characters long'],
+    },
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 

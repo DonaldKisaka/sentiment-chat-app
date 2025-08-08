c5f6e3818423a5d43319abc283ea4e0b0a23ec40
DonaldKisaka <kisakadonald1@gmail.com>
2025-08-08 14:55:01 +0300

    toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        <Toaster />
+        <Toaster position="top-right" />
         {children}
       </body>
     </html>
